6a50ac48a4805bd2e0134e291a4fd1795a6982dd
Enzo Ragazzoni <enzoragazzoni21@gmail.com>
2023-11-18 18:58:19 -0300

    refactoring react routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,10 @@ import cors from "cors";
 import Deck from "./models/Deck";
 
 import { config } from "dotenv";
+import { getDeckController } from "./controllers/getDeckController";
+import { createDeckController } from "./controllers/createDeckController";
+import { deleteDeckController } from "./controllers/deleteDeckController";
+import { createCarForDeckController } from "./controllers/createCardForDeckController";
 config();
 
 const app = express();
@@ -17,29 +21,13 @@ app.use(
 );
 app.use(express.json());
 
-app.get("/decks", async (req: Request, res: Response) => {
-  //fetch all decks and send back to the User
-  const decks = await Deck.find();
+app.get("/decks", getDeckController);
 
-  res.json(decks);
-})
+app.post("/decks", createDeckController);
 
-app.post("/decks", async (req: Request, res: Response) => {
-  const newDeck = new Deck({
-    title: req.body.title,
-  });
-  const createdDeck = await newDeck.save();
-  res.json(createdDeck);
-});
+app.delete("/decks/:deckId", deleteDeckController);
 
-app.delete("/decks/:deckId", async (req: Request, res: Response) => {
-  // getting the id from the URL
-  const deckId = req.params.deckId;
-  // delete the deck from the mongo
- const deck = await Deck.findByIdAndDelete(deckId);
-  // return the delete deck to the user who made the request
-  res.json(deck);
-})
+app.post("/decks/:deckId/cards", createCarForDeckController);
 
 mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`listening on port ${port}`);
